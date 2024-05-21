6da963ff70519dd43101d7ceef496a5b320297b4
Azlaan <azlaanirshad@gmail.com>
2024-05-21 22:21:03 +0200

    Implemented substring search for order items by name

--- src/api/src/Services/OrderService.ts
+++ src/api/src/Services/OrderService.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { ILike, Repository } from "typeorm";
 import { Order } from "../Models/Entities/Order";
 import { OrderItem } from "../Models/Entities/OrderItem";
 import { CreateOrderItemDto } from "../Models/Dto/Item/CreateOrderItemDto";
@@ -92,6 +92,6 @@ export class OrderService {
      * @returns {Promise<OrderItem[]>}
      */
     public async searchOrderItemByName(name: string): Promise<OrderItem[]> {
-        return await this.orderItemRepository.find({ where: { name } });
+        return await this.orderItemRepository.find({ where: { name: ILike(`%${name}%`) } });
     }
 }
--- src/web/src/components/OrderItemSearch.ts
+++ src/web/src/components/OrderItemSearch.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, TemplateResult, css } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
+import { OrderItem } from "@shared/types/OrderItem";
 
 @customElement("order-item-search")
 export class OrderItemSearch extends LitElement {
@@ -8,34 +9,40 @@ export class OrderItemSearch extends LitElement {
         .search-item-bar {
             padding: 10px;
             font-size: 1em;
-            width: 100%;
+            width: 90%;
         }
+    
     `;
+
+    @state()
+    private _foundOrderItems: OrderItem[] = [];
 
     public render(): TemplateResult {
         return html`
             <form @submit=${this.searchOrderItemByName}>
                 <input type="text" class="search-item-bar" placeholder="Search for an item by name" />  
-            </form>
-        `;
+            </form>`;
     }
 
     private async searchOrderItemByName(event: Event): Promise<void> {
         event.preventDefault();
         const nameInput: HTMLInputElement | null = this.shadowRoot?.querySelector(".search-item-bar") || null;
         if (nameInput) {
-            console.log("searching for order item by name");
-            const name: string | null = nameInput.value || null;
+            const name: string = nameInput.value;
             const response: Response = await fetch(`${viteConfiguration.API_URL}orderItems/search/${name}`, {
                 method: "GET",
                 headers: {
+                    "Content-Type": "application/json",
                 },
             });
-            
-            if (!response.ok) {
-                alert("Could not get order item");
+
+            if (response.ok) {
+                const orderItems: OrderItem[] = await response.json();
+                this._foundOrderItems = orderItems;
+                console.log("Found Order Items:", this._foundOrderItems);
             } else {
-                alert("Order item retrieved successfully");
+                alert("Could not get order items");
+                this._foundOrderItems = [];
             }
         }
     }
