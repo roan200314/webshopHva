4a88f045dbfc70ea5ccab1b1ee8d2529fd6cb1fc
Bram <bram@bramdekker.com>
2024-05-25 16:29:10 +0200

    Added: Support for games as order item

--- src/api/src/Modules/GamesModule.ts
+++ src/api/src/Modules/GamesModule.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Games } from "../Models/Entities/Games";
 import { GamesService } from "../Services/GamesService";
 import { GamesController } from "../Controllers/GamesController";
+import { OrderItem } from "../Models/Entities/OrderItem";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Games])],
+    imports: [TypeOrmModule.forFeature([OrderItem, Games])],
     providers: [GamesService],
     controllers: [GamesController],
     exports: [GamesService],
