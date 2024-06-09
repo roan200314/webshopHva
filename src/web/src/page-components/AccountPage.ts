b5aaea02551b968e899d580a612138d77856b9ac
Faith <faith.mengers@hva.nl>
2024-06-09 19:48:57 +0200

    Add account page component

---
+++ src/web/src/page-components/AccountPage.ts
@@ -0,0 +1,11 @@
+import { LitElement, css } from "lit";
+import { customElement } from "lit/decorators";
+
+@customElement("account-page")
+export class AccountPage extends LitElement{
+    public static styles = css`
+    `;
+
+    
+
+}
